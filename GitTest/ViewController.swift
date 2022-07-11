d9011ac1165a85cd164645dfd824cec523c60245
Vladimir Oleinikov <dragon2020@inbox.ru>
2022-07-11 20:15:46 +0300

    third commit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         //HELLO Git
         //Second commit
+        // third commit
     }
 
 
